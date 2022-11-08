7d9cde0d5ea7e6dc28f22aeec63632d70ab46e54
Almaz Kaliyev <almaz@kaliyev.dev>
2022-11-09 01:13:52 +0600

    cleanup entrypoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes as SolidRoutes } from '@solidjs/router'
+import { Route, Router, Routes as SolidRoutes } from '@solidjs/router'
 import { Component, lazy } from 'solid-js'
 
 import { Routes } from 'constants/routes'
@@ -6,7 +6,9 @@ import { Routes } from 'constants/routes'
 const MainPage = lazy(() => import('pages/MainPage'))
 
 export const App: Component = () => (
-  <SolidRoutes>
-    <Route path={Routes.main} component={MainPage} />
-  </SolidRoutes>
+  <Router>
+    <SolidRoutes>
+      <Route path={Routes.main} component={MainPage} />
+    </SolidRoutes>
+  </Router>
 )
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 /* @refresh reload */
 import 'assets/css/app.css'
 
-import { Router } from '@solidjs/router'
 import { render } from 'solid-js/web'
 
 import { App } from './App'
 
 const root = document.getElementById('root') as HTMLElement
 
-render(
-  () => (
-    <Router>
-      <App />
-    </Router>
-  ),
-  root,
-)
+render(() => <App />, root)
